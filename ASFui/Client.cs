007d986f666782d5151aff3b2ec028b757993d48
Álvaro S <alvaro.sg93@gmail.com>
2016-06-20 00:41:31 +0200

    Added info.

--- ASFui/Client.cs
+++ ASFui/Client.cs
@@ -4,26 +4,23 @@ using System.ServiceModel.Channels;
 
 namespace ASFui
 {
+    // Copied from:
+    // https://github.com/JustArchi/ArchiSteamFarm/blob/master/ArchiSteamFarm/WCF.cs
+
     [ServiceContract]
     interface IWCF
     {
         [OperationContract]
         string HandleCommand(string input);
     }
-    class Client : ClientBase<IWCF>, IWCF
+
+    internal sealed class Client : ClientBase<IWCF>, IWCF
     {
         internal Client(Binding binding, EndpointAddress address) : base(binding, address) { }
 
         public string HandleCommand(string input)
         {
-            try
-            {
-                return Channel.HandleCommand(input);
-            }
-            catch (Exception e)
-            {
-                return null;
-            }
+            return Channel.HandleCommand(input);
         }
     }
 }
