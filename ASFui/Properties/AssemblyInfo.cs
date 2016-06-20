da5d3970aede5e27bd69e5328ee930f2a32fdd1d
Álvaro S <alvaro.sg93@gmail.com>
2016-06-20 21:13:41 +0200

    v0.2.1

--- ASFui/Properties/AssemblyInfo.cs
+++ ASFui/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.2.1.0")]
+[assembly: AssemblyFileVersion("0.2.1.0")]
