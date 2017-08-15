b67bb92de2b4681203499779955072935413be8d
Álvaro S <alvaro.sg93@gmail.com>
2017-08-15 12:44:17 +0200

    Load program version from file

--- src/main/kotlin/me/alvr/asfui/Util.kt
+++ src/main/kotlin/me/alvr/asfui/Util.kt
@@ -1,14 +1,30 @@
 package me.alvr.asfui
 
-object Util {
-    fun multiToOne(input: String): String = input
-            .replace("\r\n", "\n")
-            .replace("\r", "\n")
-            .split("\n")
-            .map {
-                it.trim()
-            }
-            .filterNot {
-                it == ""
-            }.joinToString(",")
+import java.io.File
+import java.util.Properties
+
+fun String.multiToOne(): String = this
+        .replace("\r\n", "\n")
+        .replace("\r", "\n")
+        .split("\n")
+        .map {
+            it.trim()
+        }
+        .filterNot {
+            it == ""
+        }.joinToString(",")
+
+fun updateAvailable(): Boolean {
+    try {
+        val gitVersion = khttp.get("https://raw.githubusercontent.com/alvr/ASFui/master/version.txt", timeout = 10.0).text
+
+        val props = Properties()
+        File("src/main/resources/version.properties").inputStream().use { f ->
+            props.load(f)
+        }
+
+        return props.getProperty("version") >= gitVersion
+    } catch (e: Exception) {
+        return false
+    }
 }
