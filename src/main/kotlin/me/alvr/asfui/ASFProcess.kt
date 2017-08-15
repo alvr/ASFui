4f97819106beb9d7444f5f8b528f79691c1247b9
Álvaro S <alvaro.sg93@gmail.com>
2017-08-16 01:33:17 +0200

    Property

--- src/main/kotlin/me/alvr/asfui/ASFProcess.kt
+++ src/main/kotlin/me/alvr/asfui/ASFProcess.kt
@@ -1,21 +1,25 @@
 package me.alvr.asfui
 
 import java.nio.file.Path
+import javafx.beans.property.SimpleBooleanProperty
 import javafx.scene.control.TextArea
 import me.alvr.asfui.util.ConfigValues
 import org.zeroturnaround.exec.ProcessExecutor
 import org.zeroturnaround.exec.StartedProcess
 import org.zeroturnaround.exec.stream.LogOutputStream
 import tornadofx.Controller
+import tornadofx.property
 
 object ASFProcess : Controller() {
     override val configPath: Path = app.configBasePath.resolve("asfui.properties")
 
     private lateinit var process: StartedProcess
-    var started = false
+
+    private val startedProperty = SimpleBooleanProperty(this, "process", false)
+    var started by property(startedProperty)
 
     fun start(output: TextArea) {
-        if (!started) {
+        if (!started.value) {
             process = ProcessExecutor()
                     .command(config.string(ConfigValues.BINARY), "--server")
                     .redirectOutput(object : LogOutputStream() {
@@ -25,14 +29,13 @@ object ASFProcess : Controller() {
                     })
                     .destroyOnExit()
                     .start()
-            started = true
+            started.value = true
         }
     }
 
     fun stop() {
-        if (started) {
-            started = false
+        if (started.value) {
+            started.value = false
             process.process.destroy()
         }
     }
-}
