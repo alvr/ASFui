1f614624de04da8873cc7b796b0a80752cca4b0f
Álvaro S <alvaro.sg93@gmail.com>
2017-08-15 20:03:11 +0200

    Load config for process

--- src/main/kotlin/me/alvr/asfui/ASFProcess.kt
+++ src/main/kotlin/me/alvr/asfui/ASFProcess.kt
@@ -1,18 +1,23 @@
 package me.alvr.asfui
 
+import java.nio.file.Path
 import javafx.scene.control.TextArea
+import me.alvr.asfui.util.ConfigValues
 import org.zeroturnaround.exec.ProcessExecutor
 import org.zeroturnaround.exec.StartedProcess
 import org.zeroturnaround.exec.stream.LogOutputStream
+import tornadofx.Controller
 
-object ASFProcess {
+object ASFProcess : Controller() {
+    override val configPath: Path = app.configBasePath.resolve("asfui.properties")
+
     private lateinit var process: StartedProcess
     var started = false
 
     fun start(output: TextArea) {
         if (!started) {
             process = ProcessExecutor()
-                    .command(Configuration.getPropertyString(Configuration.BINARY, Configuration.BINARY_DEFAULT), "--server")
+                    .command(config.string(ConfigValues.BINARY), "--server")
                     .redirectOutput(object : LogOutputStream() {
                         override fun processLine(line: String?) {
                             output.appendText("$line\n")
