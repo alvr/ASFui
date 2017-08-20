83e89326a039ef38312bc107eb11b181b2d9f212
Álvaro S <alvaro.sg93@gmail.com>
2017-08-20 12:52:13 +0200

    Restart ASF when a new version is found

--- src/main/kotlin/me/alvr/asfui/ASFProcess.kt
+++ src/main/kotlin/me/alvr/asfui/ASFProcess.kt
@@ -1,6 +1,5 @@
 package me.alvr.asfui
 
-import java.nio.file.Path
 import javafx.beans.property.SimpleBooleanProperty
 import javafx.scene.control.TextArea
 import me.alvr.asfui.util.ConfigValues
@@ -9,20 +8,24 @@ import org.zeroturnaround.exec.StartedProcess
 import org.zeroturnaround.exec.stream.LogOutputStream
 import tornadofx.Controller
 import tornadofx.property
+import java.nio.file.Path
 
 object ASFProcess : Controller() {
     override val configPath: Path = app.configBasePath.resolve("asfui.properties")
     private lateinit var process: StartedProcess
     private val startedProperty = SimpleBooleanProperty(this, "process", false)
     var started by property(startedProperty)
+    lateinit var output: TextArea
 
     fun start(output: TextArea) {
         if (!started.value) {
+            this.output = output
             process = ProcessExecutor()
                     .command(config.string(ConfigValues.BINARY), "--server")
                     .redirectOutput(object : LogOutputStream() {
                         override fun processLine(line: String?) {
                             output.appendText("$line\n")
+                            handleOutput(line)
                         }
                     })
                     .destroyOnExit()
@@ -35,6 +38,17 @@ object ASFProcess : Controller() {
         if (started.value) {
             started.value = false
             process.process.destroy()
+        }
+    }
+
+    private fun handleOutput(line: String?) {
+        line?.let {
+            val data = line.trim()
+
+            if (data.endsWith("Update process finished!")) {
+                stop()
+                start(output)
+            }
         }
     }
 }
