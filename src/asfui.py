1d984f93743c121dcf850a417e0e24b1c98a314b
Álvaro S <alvaro.sg93@gmail.com>
2017-11-06 12:45:40 +0100

    Catch all exceptions and show a message box

--- src/asfui.py
+++ src/asfui.py
@@ -1,8 +1,25 @@
 import sys
+import traceback
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMessageBox
 
+from utils.message_error import MessageError
 from views.main import ASFui
+
+sys._excepthook = sys.excepthook
+
+
+def show_error(te, v, tb):
+    error = MessageError()
+    error.setFixedSize(300, 250)
+    error.setIcon(QMessageBox.Critical)
+    error.setWindowTitle('An error ocurred ({})'.format(te.__name__))
+    error.setText(str(v))
+    error.setDetailedText(''.join(traceback.format_tb(tb)))
+    error.exec_()
+
+
+sys.excepthook = show_error
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
